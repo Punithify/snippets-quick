9ba0f40a5c64fc05bc14bf90bbb029442e948e7e
Punith <punith@pop-os.localdomain>
2021-03-12 18:26:02 +0530

    auth0 integration

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Unvernisable Snippetsp</title>
+        <title>Unvernisable Snippets</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
